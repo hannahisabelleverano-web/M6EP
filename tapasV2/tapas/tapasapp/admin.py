9364a7e1f8c10c17728f75be3e88a53d4acec3e8
GedChua <gedric.jansen.chua@student.ateneo.edu>
2026-04-10 21:24:44 +0800

    made a venv

--- tapasV2/tapas/tapasapp/admin.py
+++ tapasV2/tapas/tapasapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Dish
+from .models import Account, Dish
 
 # Register your models here.
+admin.site.register(Account)
 admin.site.register(Dish)
--- tapasV2/tapas/tapasapp/models.py
+++ tapasV2/tapas/tapasapp/models.py
@@ -5,10 +5,22 @@ from django.db import models
 class Account(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=30)
+
+    def getUsername(self):
+        return self.username
+    
+    def getPassword(self):
+        return self.password
+
+    def __str__(self):
+        return str(self.pk) + ": " + self.name + self.password
+
+class Dish(models.Model):
+    name = models.CharField(max_length=300)
+    prep_time = models.IntegerField()
     cook_time = models.IntegerField()
     objects = models.Manager()
 
     def __str__(self):
         return str(self.pk) + ": " + self.name
-
     
