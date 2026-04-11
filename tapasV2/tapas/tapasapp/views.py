edb59fad8cc2ecf19b0821c0402afa668c9b89bf
hannahisabelleverano-web <hannah.isabelle.verano@student.ateneo.edu>
2026-04-11 13:10:34 +0800

    fixed importing

--- tapasV2/tapas/tapasapp/views.py
+++ tapasV2/tapas/tapasapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404, import logout import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout 
+from django.contrib.auth.models import User
 from .models import Dish 
 
 # Create your views here.
