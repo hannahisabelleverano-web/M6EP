91bd3dff3050c3f064d426491614346383cc663f
carmenfid <carmen.francesca.duran@student.ateneo.edu>
2026-04-12 19:01:56 +0800

    debugging

--- tapasV2/tapas/tapasapp/views.py
+++ tapasV2/tapas/tapasapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404, import logout import User
-from .models import Dish 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from .models import Account, Dish 
 
 # Create your views here.
 
