ccd64e0685079cdcc9809c2ab5826902d985f6b7
GedChua <gedric.jansen.chua@student.ateneo.edu>
2026-04-13 12:00:27 +0800

    Did g and added url.py to app

---
+++ M5P-DjangoTemplates/M5P-DjangoTemplates-CaparasChuaSy/MyInventorySystem/MyInventoryApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.login, name='login_page'),
+    path('signup/',views.signup_view, name='signup'),
+    path('basic_list/<int:pk>/', views.basic_list, name='basic_list'),
+    path('manage_account/<int:pk>', views.manage_account, name='manage_account'),
+    path('change_password/<int:pk>/', views.change_password, name='change_password'),
+    path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add_menu', views.add_menu, name='add_menu'),
+    path('view_detail/<int:pk>/', views.view_detail, name='view_detail'),
+    path('delete_dish/<int:pk>/', views.delete_dish, name='delete_dish'),
+    path('update_dish/<int:pk>/', views.update_dish, name='update_dish'),
+]
--- M5P-DjangoTemplates/M5P-DjangoTemplates-CaparasChuaSy/MyInventorySystem/MyInventorySystem/urls.py
+++ M5P-DjangoTemplates/M5P-DjangoTemplates-CaparasChuaSy/MyInventorySystem/MyInventorySystem/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from MyInventoryApp import views
 
 urlpatterns = [
-    path('', views.login_view, name='login')
-    path('signup', views.signupview, name='signup')
     path('admin/', admin.site.urls),
-    path('view_supplier', views.view_supplier, name='view_supplier'),
-    path('view_bottles', views.view_bottles, name='view_bottles'),
-    path('add_bottle', views.add_bottle, name='add_bottle'),
+    path('', include('MyInventoryApp.urls')),
 ]
