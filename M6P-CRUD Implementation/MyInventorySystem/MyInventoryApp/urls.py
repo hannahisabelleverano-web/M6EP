bdaed7b9c0e289a4d47d3d3021fe43ea53672193
GedChua <gedric.jansen.chua@student.ateneo.edu>
2026-04-15 21:46:03 +0800

    small change

--- M6P-CRUD Implementation/MyInventorySystem/MyInventoryApp/urls.py
+++ M6P-CRUD Implementation/MyInventorySystem/MyInventoryApp/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login_view, name='login_page'),
+    path('', views.login_view, name='login'),
     path('signup/',views.signup_view, name='signup'),
-    path('basic_list/<int:pk>/', views.basic_list, name='basic_list'),
     path('manage_account/<int:pk>', views.manage_account, name='manage_account'),
     path('change_password/<int:pk>/', views.change_password, name='change_password'),
     path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
